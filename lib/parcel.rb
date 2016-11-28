bba0051a39aa2928ab03f1b2bd9fde04828b07a4
Eric Bomblatus and Kevin Finley <eric+kevin@>
2016-11-28 11:22:39 -0800

    write second spec and pass

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -1,12 +1,17 @@
 class Parcel
-  def initialize(wieght, width, hieght, miles)
+  def initialize(wieght, width, hieght, length, miles)
     @wieght = wieght
     @width =  width
     @hieght = hieght
+    @length = length
     @miles = miles
   end
 
   def wieght_cost
     @wieght * 2
+  end
+
+  def size_cost
+     @width * @hieght * @length /100
   end
 end
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -4,8 +4,22 @@ require "rspec"
 describe(Parcel) do
   describe('#wieght_cost') do
     it "will return the cost based on parcel wieght" do
-      new_wieght = Parcel.new(4)
+      new_wieght = Parcel.new(4, 0, 0, 0, 0)
       expect(new_wieght.wieght_cost()).to(eq(8))
+    end
+  end
+
+  describe('#size_cost') do
+    it "will return the cost based on parcel size" do
+      new_size = Parcel.new(0, 12 ,12, 12, 0)
+      expect(new_size.size_cost()).to(eq(17))
+    end
+  end
+
+  describe('#size_cost') do
+    it "will return the cost based on parcel size" do
+      new_size = Parcel.new(0, 12 ,12, 12, 0)
+      expect(new_size.size_cost()).to(eq(17))
     end
   end
 end
